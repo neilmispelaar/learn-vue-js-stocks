076f50d0cfc938b5eb605b51638ac4959afdb544
Neil Mispelaar <neil.mispelaar@outlook.com>
2020-02-13 14:46:23 -0500

    Update Invest Page

--- src/components/InvestStock.vue
+++ src/components/InvestStock.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="w-1/2 px-4 py-4">
     <div class="bg-blue-100  border border-gray-400 rounded-sm p-4 shadow-xl">
-      <h1 class="text-xl font-extrabold"><span class="sr-only">Stock name: </span>{{ name }}</h1>
-      <p class="text-6xl"><span class="sr-only">Price: </span>{{ price | formatCurrency }}</p>
+      <h1 class="text-xl font-extrabold"><span class="sr-only">Stock name: </span>{{ stock.name }}</h1>
+      <p class="text-6xl"><span class="sr-only">Price: </span>{{ stock.price | formatCurrency }}</p>
       <div class="flex flex-wrap w-full">
         <div class="w-full">
           <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-first-name">
@@ -10,10 +10,14 @@
           </label>
         </div>
         <div class="flex-grow mr-3">
-          <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="number" placeholder="0" min="0" max="100">
+          <input v-model="quantity" class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="number" placeholder="0" min="0" max="100">
         </div>
         <div class="self-center">
-          <button class="bg-green-600 hover:bg-green-800 text-white font-bold py-3 px-8 rounded">Buy</button>
+          <button
+          class="bg-green-600 hover:bg-green-800 text-white font-bold py-3 px-8 rounded"
+          :disabled="disabled"
+          v-on:click="buyShares">
+          Buy</button>
         </div>
       </div>
     </div>
@@ -23,15 +27,44 @@
 <script>
 export default {
   name: 'Invest-Stock',
+  data: function () {
+    return {
+      quantity: 0
+    }
+  },
+  computed: {
+    disabled: function () {
+      var disabled = true
+      if (isNaN(this.quantity)) {
+        // Not a number
+        disabled = true
+      } else {
+        // Is a number
+        if (this.quantity <= 0) {
+          disabled = true
+        } else {
+          disabled = false
+        }
+      }
+      return disabled
+    }
+  },
+  methods: {
+    buyShares: function () {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        stockQuantity: this.quantity
+      }
+      console.log(order)
+      // Return quantity back to zero
+      this.quantity = 0
+    }
+  },
   props: {
     // Number with a default value
-    price: {
-      type: Number,
-      default: 0,
-      required: true
-    },
-    name: {
-      type: String,
+    stock: {
+      type: Object,
       required: true
     }
   }
@@ -40,5 +73,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+button:disabled {
+  background-color: #9ae6b4;
+}
 </style>
--- src/views/Invest.vue
+++ src/views/Invest.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="invest">
     <div class="bg-blue-100 border border-gray-400 rounded-sm p-4 shadow-xl mb-4">
-      <p>Cash balance: <span class="font-bold">{{ portfolio.balance | formatCurrency }}</span></p>
+      <p>Cash balance: <span class="font-bold">{{ balance | formatCurrency }}</span></p>
     </div>
     <div v-if="stocks.length <= 0" class="text-center">
       <p class="h3 mt-3">No Stocks</p>
@@ -14,8 +14,7 @@
           v-bind:item="stock"
           v-bind:index="index"
           v-bind:key="index"
-          v-bind:name="stock.name"
-          v-bind:price="stock.price"
+          v-bind:stock="stock"
         ></Stock>
       </div>
     </div>
@@ -29,27 +28,15 @@ import Stock from '@/components/InvestStock.vue'
 export default {
   data: function () {
     return {
-      portfolio: {
-        balance: 0
-      },
-      stocks: [
-        {
-          name: 'BMW',
-          price: 10.34
-        },
-        {
-          name: 'Mercedes',
-          price: 20
-        },
-        {
-          name: 'Ford',
-          price: 30
-        },
-        {
-          name: 'Tesla',
-          price: 30
-        }
-      ]
+    }
+  },
+  computed: {
+    stocks: function () {
+      return this.$store.getters.stocks
+    },
+    balance: function () {
+      console.log('Balance ' + this.$store.getters.balance)
+      return this.$store.getters.balance
     }
   },
   components: {
