e91459e665b5980bd22211e8fe92f5a065844b78
Neil Mispelaar <neil.mispelaar@outlook.com>
2020-02-21 14:30:57 -0500

    Add new colours to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'anti-flash-white': '#E8F5F1',
+        'mint-cream' : '#F6FBFB',
+        'sea-serpent' : '#69CACF',
+        'verdigris' : '#41B6B8',
+        'celadon-green' : '#2C8A8B'
+      }
+    }
   },
   variants: {},
   plugins: []
