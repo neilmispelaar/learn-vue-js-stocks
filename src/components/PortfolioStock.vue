63f49d5f3ad3a2ad2debfce89bbe29bffe3ea79b
Neil Mispelaar <neil.mispelaar@outlook.com>
2020-02-29 12:17:55 -0500

    Update look of portfolio stock card

--- src/components/PortfolioStock.vue
+++ src/components/PortfolioStock.vue
@@ -1,23 +1,31 @@
 <template>
-  <div class="w-full bg-blue-200 rounded-lg my-10 p-5">
-    <h1 class="text-xl font-extrabold"><span class="sr-only">Stock name: </span>{{ name }}</h1>
-    <p class="text-6xl font-thin"><span class="sr-only">Value </span>{{ value | formatCurrency }}</p>
-    <p class="text-xs">{{ price | formatCurrency }} Share | QTY: {{ holding.quantity }}</p>
-    <div class="flex flex-wrap w-full mt-3 bg-blue-100 rounded-lg p-4">
-      <div class="w-full">
-        <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-first-name">
-          Sell shares
-        </label>
+  <div>
+    <div class="w-full bg-blue-200 rounded-lg my-10 p-5">
+      <div class="flex flex-row flex-no-wrap">
+        <div class="flex flex-col">
+          <h1 class="text-6xl font-extrabold"><span class="sr-only">Stock name: </span>{{ name }}</h1>
+          <p class="text-xs">{{ price | formatCurrency }} Share | QTY: {{ holding.quantity }}</p>
+        </div>
+        <div class=" flex-grow">
+          <p class="text-6xl font-thin text-right"><span class="sr-only">Value </span>{{ value | formatCurrency }}</p>
+        </div>
       </div>
-      <div class="flex-grow mr-3">
-        <input v-model="quantity" class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="number" placeholder="0" min="0" :max="holding.quantity">
-      </div>
-      <div class="self-center">
-        <button
-        class="bg-red-600 hover:bg-red-800 text-white font-bold py-3 px-8 rounded"
-        :disabled="disabled"
-        v-on:click="sellStocks">
-        Sell</button>
+      <div class="flex flex-wrap w-full mt-3 bg-blue-100 rounded-lg p-4">
+        <div class="w-full">
+          <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-first-name">
+            Sell shares
+          </label>
+        </div>
+        <div class="flex-grow mr-3">
+          <input v-model="quantity" class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="number" placeholder="0" min="0" :max="holding.quantity">
+        </div>
+        <div class="self-center">
+          <button
+          class="bg-red-600 hover:bg-red-800 text-white font-bold py-3 px-8 rounded"
+          :disabled="disabled"
+          v-on:click="sellStocks">
+          Sell</button>
+        </div>
       </div>
     </div>
   </div>
