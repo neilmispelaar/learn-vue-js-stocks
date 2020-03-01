377468fe0328ac1d85648ca21e2396b72010e1ee
Neil Mispelaar <neil.mispelaar@outlook.com>
2020-02-29 21:19:58 -0500

    Update PurgeCSS setup

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,7 @@
 // postcss.config.js
+//
+// PurgeCSS setup care of the following:
+// https://markus.oberlehner.net/blog/setting-up-tailwind-css-with-vue/
 
 const purgecss = require('@fullhuman/postcss-purgecss')({
   // Specify the paths to all of the template files in your project 
@@ -11,7 +14,13 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
   ],
 
   // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+  // See: https://tailwindcss.com/docs/controlling-file-size/#understanding-the-regex
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+
+  // Whitelist auto generated classes for transitions and router links.
+  // From: https://github.com/ky-is/vue-cli-plugin-tailwind
+  whitelistPatterns: [/-(leave|enter|appear)(|-(to|from|active))$/, /^(?!(|.*?:)cursor-move).+-move$/, /^router-link(|-exact)-active$/],
+
 });
 
 module.exports = {
